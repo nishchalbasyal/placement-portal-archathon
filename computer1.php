eabb26f634bcd6887bee7661143e1c7a7bb0294a
ajha417 <ajha417@rku.ac.in>
2022-03-11 20:09:12 +0530

    update question

--- computer1.php
+++ computer1.php
@@ -6,15 +6,45 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
 </head>
+<style>
+   .btn{
+       background-color: cyan;
+       height: 40px;
+       border-radius: 5px;
+       cursor: pointer;
+   }
+</style>
 <body>
+    <form action="result.php">
 <div class="questions">  
                 <p>1.A computer network permits sharing of</p>
                 A. <input type="radio" name="q1" id="value1">Resources <br>
                 B. <input type="radio" name="q1" id="value2">Information <br>
                 C. <input type="radio" name="q1" id="value3">Both a&b <br>
-                D. <input type="radio" name="q1" id="value4">None of these
+                D. <input type="radio" name="q1" id="value4">None of these <br>
 
-                </div>
+                <p>2.Network of networks is known as________</p>
+                A. <input type="radio" name="q2" id="value5">internet <br>
+                B. <input type="radio" name="q2" id="value6">intranet <br>
+                C. <input type="radio" name="q2" id="value7">both a and b <br>
+                D. <input type="radio" name="q2" id="value8">None of these<br>
 
+                <p>3.Which of the following statement about functions of false?</p>
+                A. <input type="radio" name="q3" id="value9">A function can call itself. <br>
+                B. <input type="radio" name="q3" id="value10">A function can call another function. <br>
+                C. <input type="radio" name="q3" id="value11">Constants can appear in the formal argument list. <br>
+                D. <input type="radio" name="q3" id="value12"> More than one function is allowed in a program unit.
+
+<br>
+                
+                <p>4.Number of primitive data types in Java are?</p>
+                A. <input type="radio" name="q4" id="value13">6 <br>
+                B. <input type="radio" name="q4" id="value14">7 <br>
+                C. <input type="radio" name="q4" id="value15">8 <br>
+                D. <input type="radio" name="q4" id="value16">9 <br>
+                
+                </div><br><br>
+                <button class="btn">Finish & Submit</button>
+        </form>
 </body>
 </html>
