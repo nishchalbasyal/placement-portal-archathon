4c9d799363c0e00e78442a485b840d596926e64a
nischalbasyal <60071083+nischalbasyal@users.noreply.github.com>
2022-03-11 14:35:47 +0530

    db Connected
    Updated

---
+++ connection.php
@@ -0,0 +1,4 @@
+<?php 
+    $db =  new mysqli("localhost:8090","root","");
+      
+?>
--- partials/_nav.php
+++ partials/_nav.php
@@ -34,7 +34,8 @@
       <form class="d-flex">
         <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
         <button class="btn btn-outline-success" type="submit">Search</button>
-      </form>
+      
+        </form>
     </div>
     
   </div>
