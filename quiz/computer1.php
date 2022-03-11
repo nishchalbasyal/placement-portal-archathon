d3205315a562fd95b1143ca4f81ea429ebf8fcf0
Suman-Neupane <itsmesumannpn@gmail.com>
2022-03-11 22:35:02 +0530

    edit

--- quiz/computer1.php
+++ quiz/computer1.php
@@ -11,38 +11,38 @@
 <div class="container mt-sm-5 my-1">
                 <div class="questions">  
                 <p>1.Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut vel varius justo, sit amet imperdiet nisl. Sed id feugiat purus, sed sollicitudin sem. </p>
-                <input type="radio" name="q1" id="value1"> A. gravida<br>
-                <input type="radio" name="q1" id="value2"> B. vestibulum  <br>
-                <input type="radio" name="q1" id="value3"> C. imperdiet  <br>
-                <input type="radio" name="q1" id="value4"> D. volutpat  <br><br>
+                <input type="radio" name="q1" id="1"> A. gravida<br>
+                <input type="radio" name="q1" id="2"> B. vestibulum  <br>
+                <input type="radio" name="q1" id="3"> C. imperdiet  <br>
+                <input type="radio" name="q1" id="4"> D. volutpat  <br><br>
                 <hr>
 
                 </div>
                 <div class="questions">  
                 <p>2.Sed blandit eu mi nec accumsan. Cras vel tincidunt ipsum. Morbi lobortis convallis augue, in bibendum lorem. </p>
-                 <input type="radio" name="q2" id="value1"> A. Aliquam  <br>
-                 <input type="radio" name="q2" id="value2"> B. convallis  <br>
-                 <input type="radio" name="q2" id="value3"> C. nunc  <br>
-                 <input type="radio" name="q2" id="value4"> D. vestibulum  <br><br>
+                 <input type="radio" name="q2" id="5"> A. Aliquam  <br>
+                 <input type="radio" name="q2" id="6"> B. convallis  <br>
+                 <input type="radio" name="q2" id="7"> C. nunc  <br>
+                 <input type="radio" name="q2" id="8"> D. vestibulum  <br><br>
                 <hr>
 
                 </div>
                 
                 <div class="questions">  
                 <p>3.Phasellus enim quam, semper a nisi a, lacinia ullamcorper eros. Aliquam lobortis, sapien ut interdum accumsan </p>
-                 <input type="radio" name="q3" id="value1"> A. quis  <br>
-                 <input type="radio" name="q3" id="value2"> B. Nullam  <br>
-                 <input type="radio" name="q3" id="value3"> C. varius  <br>
-                 <input type="radio" name="q3" id="value4"> D. posuere  <br><br>
+                 <input type="radio" name="q3" id="9"> A. quis  <br>
+                 <input type="radio" name="q3" id="10"> B. Nullam  <br>
+                 <input type="radio" name="q3" id="11"> C. varius  <br>
+                 <input type="radio" name="q3" id="12"> D. posuere  <br><br>
                 <hr>
                 </div>
 
                 <div class="questions">  
                 <p>4.Maecenas quis tellus ut ante porttitor convallis. Duis a justo tortor. Proin consequat volutpat gravida. Vestibulum </p>
-                 <input type="radio" name="q4" id="value1"> A. vehicula  <br>
-                 <input type="radio" name="q4" id="value2"> B. Duis  <br>
-                 <input type="radio" name="q4" id="value3"> C. mi <br>
-                 <input type="radio" name="q4" id="value4"> D. faucibus <br><br>
+                 <input type="radio" name="q4" id="13> A. vehicula  <br>
+                 <input type="radio" name="q4" id="14"> B. Duis  <br>
+                 <input type="radio" name="q4" id="15"> C. mi <br>
+                 <input type="radio" name="q4" id="16"> D. faucibus <br><br>
                 <hr>
                 </div>
                 <div class="ml-auto mr-sm-5"> <button class="btn btn-success">Submit</button> </div>
