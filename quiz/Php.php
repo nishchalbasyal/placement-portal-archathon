d68ddb29c9a92ac6edb7852550323ee6ca45b7ae
Suman-Neupane <itsmesumannpn@gmail.com>
2022-03-11 20:29:41 +0530

    QUIZS

---
+++ quiz/Php.php
@@ -0,0 +1,89 @@
+<?php
+session_start();
+if (!isset($_SESSION['loggedin']) || $_SESSION['loggedin'] != true) {
+    header("location: login.php");
+    exit;
+}
+
+
+$show = false;
+?>
+<!doctype html>
+<html lang="en">
+
+<head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <!-- Bootstrap CSS -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+
+    <title>Hello</title>
+    <style>
+        .center {
+            text-align: center;
+            font-weight: bolder;
+            font-size: larger;
+        }
+
+        .rules {
+            font-weight: bold;
+        }
+       
+    </style>
+   
+</head>
+
+<body>
+
+    <!-- <h1>Hello, world!</h1> -->
+    <div class="alert alert-primary" role="alert" style="text-align:center;">
+        All the best <?php echo $_SESSION['username']; ?>
+    </div>
+    <form action='php1.php'>
+        <div class="info">
+            <p> Our php1 questions focuses on all areas of php1.
+                These questions are chosen from the collection of authoritative and best reference books
+            </p>
+            <p class="center">
+                Multiple choise Test
+            </p>
+            <p class="rules">
+                Rules and Regulations
+            </p>
+            <ul>
+                <li>The given times for this quiz is 45 minutes.After that ,the form will be closed for you.</li>
+                <li>Students who will not submit their answers in time then it will be automatically fetched.</li>
+                <li>You cannot change your answer after submission.</li>
+                <li>Each question has its own grading points.After you submit your answer we will evaluate your answers and let you know your grades later </li>
+                <li>If you have any technical problem during the quiz,please take screenshot or screen recording and send us.</li>
+                <!-- <li></li> -->
+            </ul>
+        <div class="submit">
+            <input type="submit" value="Start your test now">
+        </div>
+            
+
+          
+
+        </div>
+
+
+    </form>
+
+    <!-- Optional JavaScript; choose one of the two! -->
+
+    <!-- Option 1: Bootstrap Bundle with Popper -->
+    
+
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
+
+    <!-- Option 2: Separate Popper and Bootstrap JS -->
+    <!--
+    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.10.2/dist/umd/popper.min.js" integrity="sha384-7+zCNj/IqJ95wo16oMtfsKbZ9ccEh31eOz1HGyDuCQ6wgnyJNSYdrPa03rtR1zdB" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.min.js" integrity="sha384-QJHtvGhmr9XOIpI6YVutG+2QOK9T+ZnN4kzFN1RtK3zEFEIsxhlmWl5/YESvpZ13" crossorigin="anonymous"></script>
+    -->
+</body>
+
+</html>
---
+++ quiz/javascript1.php
@@ -0,0 +1,55 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
+</head>
+<body>
+<div class="container mt-sm-5 my-1">
+                <div class="questions">  
+                <p>1.Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut vel varius justo, sit amet imperdiet nisl. Sed id feugiat purus, sed sollicitudin sem. </p>
+                A. <input type="radio" name="q1" id="value1">gravida<br>
+                B. <input type="radio" name="q1" id="value2">vestibulum  <br>
+                C. <input type="radio" name="q1" id="value3">imperdiet  <br>
+                D. <input type="radio" name="q1" id="value4">volutpat  <br><br>
+                <hr>
+
+                </div>
+                <div class="questions">  
+                <p>2.Sed blandit eu mi nec accumsan. Cras vel tincidunt ipsum. Morbi lobortis convallis augue, in bibendum lorem. </p>
+                A. <input type="radio" name="q2" id="value1">Aliquam  <br>
+                B. <input type="radio" name="q2" id="value2">convallis  <br>
+                C. <input type="radio" name="q2" id="value3">nunc  <br>
+                D. <input type="radio" name="q2" id="value4">vestibulum  <br><br>
+                <hr>
+
+                </div>
+                
+                <div class="questions">  
+                <p>3.Phasellus enim quam, semper a nisi a, lacinia ullamcorper eros. Aliquam lobortis, sapien ut interdum accumsan </p>
+                A. <input type="radio" name="q3" id="value1">quis  <br>
+                B. <input type="radio" name="q3" id="value2">Nullam  <br>
+                C. <input type="radio" name="q3" id="value3">varius  <br>
+                D. <input type="radio" name="q3" id="value4">posuere  <br><br>
+                <hr>
+                </div>
+
+                <div class="questions">  
+                <p>4.Maecenas quis tellus ut ante porttitor convallis. Duis a justo tortor. Proin consequat volutpat gravida. Vestibulum </p>
+                A. <input type="radio" name="q4" id="value1">vehicula  <br>
+                B. <input type="radio" name="q4" id="value2">Duis  <br>
+                C. <input type="radio" name="q4" id="value3">mi <br>
+                D. <input type="radio" name="q4" id="value4">faucibus <br><br>
+                <hr>
+                </div>
+                <div class="ml-auto mr-sm-5"> <button class="btn btn-success">Submit</button> </div>
+                </div>
+</div>
+
+                
+
+</body>
+</html>
