57bbce1973d31b41261cfd85b4f29a922b713a92
Suman-Neupane <itsmesumannpn@gmail.com>
2022-03-11 17:57:05 +0530

    profile add

--- partials/_nav.php
+++ partials/_nav.php
@@ -29,17 +29,18 @@
        echo ' <li class="nav-item">
           <a class="nav-link" href="logout.php">logout</a>
         </li>
-        <li class="nav-item dropdown">
-          <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-            Dropdown
+        <li class="nav-item dropdown ">
+          <a class="nav-link dropdown-toggle " href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+            Username
           </a>
           <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
-            <li><a class="dropdown-item" href="#">Action</a></li>
-            <li><a class="dropdown-item" href="#">Another action</a></li>
+            <li><a class="dropdown-item" href="profile.php">Your Profile</a></li>
+            <li><a class="dropdown-item" href="#">Logout</a></li>
             <li><hr class="dropdown-divider"></li>
             <li><a class="dropdown-item" href="#">Something else here</a></li>
           </ul>
-        </li>';
+        </li>
+         ';
         // echo '';
       }
       
